b1b020fab6789e2f62be126da2f93a09df62a3a8
Rajesh Choudhary <rajuvert18@gmail.com>
2024-05-25 16:29:03 +0530

    chat scroll view

---
+++ src/app/(root)/chat/[id]/page.tsx
@@ -0,0 +1,60 @@
+import BottomInputBar from "@/components/BottomInputBar";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import React from "react";
+
+const Page = () => {
+  return (
+    <main className="flex relative  h-[calc(100svh-56px)] py-4 w-full gap-12 max-w-3xl  mx-auto flex-col items-center  px-2 ">
+      <ScrollArea className="bg-secondary w-full h-[calc(100%-90px)] rounded-2xl p-2">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem culpa
+        dolores eaque, esse reiciendis, necessitatibus mollitia consequuntur
+        quisquam maxime totam voluptatem dolorum voluptate. Laboriosam nesciunt
+        nihil quaerat, veritatis optio sed! Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Dicta, nostrum reiciendis unde expedita
+        nulla repudiandae doloribus hic vero quia cupiditate mollitia rerum
+        saepe est iure aliquam officiis veritatis praesentium. Expedita
+        possimus, amet quisquam maiores tempore molestiae nam dignissimos unde
+        vero voluptatibus natus aliquam cumque officia magni itaque! Nobis
+        officia doloremque saepe cum fugiat, quam natus, similique sint,
+        exercitationem odio dolores itaque minus rem minima quasi deserunt
+        sapiente tenetur autem eum sequi veniam. Quas molestias vero dolorem
+        perspiciatis voluptas aut ducimus neque, suscipit repudiandae aspernatur
+        quis assumenda eligendi consequatur exercitationem, numquam culpa
+        cupiditate natus hic facere quasi harum itaque ratione est! l hwllo
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem culpa
+        dolores eaque, esse reiciendis, necessitatibus mollitia consequuntur
+        quisquam maxime totam voluptatem dolorum voluptate. Laboriosam nesciunt
+        nihil quaerat, veritatis optio sed! Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Dicta, nostrum reiciendis unde expedita
+        nulla repudiandae doloribus hic vero quia cupiditate mollitia rerum
+        saepe est iure aliquam officiis veritatis praesentium. Expedita
+        possimus, amet quisquam maiores tempore molestiae nam dignissimos unde
+        vero voluptatibus natus aliquam cumque officia magni itaque! Nobis
+        officia doloremque saepe cum fugiat, quam natus, similique sint,
+        exercitationem odio dolores itaque minus rem minima quasi deserunt
+        sapiente tenetur autem eum sequi veniam. Quas molestias vero dolorem
+        perspiciatis voluptas aut ducimus neque, suscipit repudiandae aspernatur
+        quis assumenda eligendi consequatur exercitationem, numquam culpa
+        cupiditate natus hic facere quasi harum itaque ratione est! l hwllo
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem culpa
+        dolores eaque, esse reiciendis, necessitatibus mollitia consequuntur
+        quisquam maxime totam voluptatem dolorum voluptate. Laboriosam nesciunt
+        nihil quaerat, veritatis optio sed! Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Dicta, nostrum reiciendis unde expedita
+        nulla repudiandae doloribus hic vero quia cupiditate mollitia rerum
+        saepe est iure aliquam officiis veritatis praesentium. Expedita
+        possimus, amet quisquam maiores tempore molestiae nam dignissimos unde
+        vero voluptatibus natus aliquam cumque officia magni itaque! Nobis
+        officia doloremque saepe cum fugiat, quam natus, similique sint,
+        exercitationem odio dolores itaque minus rem minima quasi deserunt
+        sapiente tenetur autem eum sequi veniam. Quas molestias vero dolorem
+        perspiciatis voluptas aut ducimus neque, suscipit repudiandae aspernatur
+        quis assumenda eligendi consequatur exercitationem, numquam culpa
+        cupiditate natus hic facere quasi harum itaque ratione est! l
+      </ScrollArea>
+      <BottomInputBar />
+    </main>
+  );
+};
+
+export default Page;
