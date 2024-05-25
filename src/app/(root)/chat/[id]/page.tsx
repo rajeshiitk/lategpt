a216b12ad3027722c9a87cfebe50877136c502a1
Rajesh Choudhary <rajuvert18@gmail.com>
2024-05-25 19:23:55 +0530

    layout fix

--- src/app/(root)/chat/[id]/page.tsx
+++ src/app/(root)/chat/[id]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import BottomInputBar from "@/components/BottomInputBar";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import React from "react";
 import { useChat } from "ai/react";
 import Markdown from "@/components/markdown";
@@ -21,13 +20,13 @@ const Page = () => {
   } = useChat();
 
   return (
-    <main className="flex relative  h-[calc(100svh-56px)] py-4 w-full gap-12 mx-auto max-w-7xl flex-col items-center  px-2 ">
+    <main className="flex relative  h-[calc(100svh-56px)] py-4 w-full gap-12 mx-auto max-w-5xl flex-col items-center  px-2 ">
       {error && (
         <div className="fixed top-0 left-0 w-full p-4 text-center text-white bg-red-500">
           {error.message}
         </div>
       )}
-      <div className="  text-wrap break-all w-full h-[calc(100%-90px)] custom-scrollbar  overflow-auto  relative   px-2">
+      <div className="  text-wrap break-all w-full h-[calc(100%-90px)] custom-scrollbar  overflow-auto  relative   sm:px-2">
         {messages.map((message, index) => (
           <div key={index} className="break-words text-wrap flex flex-col  ">
             <div
