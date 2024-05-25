0d758c9df3477d1d268aa5cfb4e9d0deded2698f
Rajesh Choudhary <rajuvert18@gmail.com>
2024-05-25 17:14:53 +0530

    generative-ai integration and chat route implementation

--- src/components/BottomInputBar.tsx
+++ src/components/BottomInputBar.tsx
@@ -3,13 +3,7 @@ import Image from "next/image";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormMessage,
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
